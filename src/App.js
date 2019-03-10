2f5191681f2e2d9045ca19c2ba057daadec2611c
danielmoralesp <daniel_moralesp@hotmail.com>
2019-03-10 15:41:25 -0500

    Solution

--- src/App.js
+++ src/App.js
@@ -2,11 +2,23 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { value: 0 };
+    this.update = this.update.bind(this)
+  }
+
+  update(){
+    let newValue = this.state.value += 1
+
+    this.setState({ value: newValue })
+  }
+
   render() {
     return (
       <div>
-        <span className="value">0</span>
-        <button id="inc">Incrementa</button>
+        <span className="value">{this.state.value}</span>
+        <button id="inc" onClick={this.update}>Incrementa</button>
       </div>
     );
   }
